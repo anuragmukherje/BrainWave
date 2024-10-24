678072345a0686d5046d872fb263d1563194f91b
Anurag Mukherjee <amdevremote@gmail.com>
2024-10-24 23:07:42 +0530

    New commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import { brainwave } from "../assets";
+
+import { navigation } from "../constants";
+
 const Header = () => {
   return (
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
@@ -9,7 +12,11 @@ const Header = () => {
 
         <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-            1 2 3
+            {navigation.map((item) => (
+              <a key={item.id} href={item.url}>
+                {item.title}
+              </a>
+            ))}
           </div>
         </nav>
       </div>
